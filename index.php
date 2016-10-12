d32ba18c9a79fa53d3fad42fdb78bf3b34861037
Martin <martin.onton@gmail.com>
2016-10-12 23:07:22 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/* secured-php-login
+ * PHP login and registration scripts that use up to date password hashing and salting.
+ * Proper security againts SQL injections.
+ * @author MartinoEst
+ * @link https://github.com/martinoest/secured-php-login/
+ * @license http://opensource.org/licenses/MIT MIT License
+ * 
+ * Minimum 5.6 PHP version required!
+ */
+   
+
+/* Require login.php to call login function */
+require_once("functions/login.php");
+
+/* Call for login function */
+$login = new Login();
+
+?>
